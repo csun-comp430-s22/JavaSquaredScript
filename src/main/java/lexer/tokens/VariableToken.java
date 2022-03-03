70d474d9f3e07c17f2445edae64dc736ed61925d
jjb4547 <jesus.banuelos.951@my.csun.edu>
2022-03-02 23:05:35 -0800

    Added the VariableToken

---
+++ src/main/java/lexer/tokens/VariableToken.java
@@ -0,0 +1,27 @@
+package lexer.tokens;
+public class VariableToken implements Token {
+    public final String name;
+
+    public VariableToken(final String name) {
+        this.name = name;
+    }
+
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    public String toString() {
+        // name: foo
+        // Output: Variable(foo)
+        return "Variable(" + name + ")";
+    }
+
+    public boolean equals(final Object other) {
+        if (other instanceof VariableToken) {
+            final VariableToken asVar = (VariableToken)other;
+            return name.equals(asVar.name);
+        } else {
+            return false;
+        }
+    }
+}
