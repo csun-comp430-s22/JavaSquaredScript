95419331bbbee89b2135daec197826f5418f5f2f
jjb4547 <jesus.banuelos.951@my.csun.edu>
2022-03-09 12:40:10 -0800

    Added the ConstructorToken.

---
+++ src/main/java/lexer/tokens/ConstructorToken.java
@@ -0,0 +1,14 @@
+package lexer.tokens;
+public class ConstructorToken implements Token{
+    public boolean equals(final Object other) {
+        return other instanceof ConstructorToken;
+    }
+
+    public int hashCode() {
+        return 36;
+    }
+
+    public String toString() {
+        return "constructor";
+    }
+}
