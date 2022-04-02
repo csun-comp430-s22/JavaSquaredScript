0f87a95e8b8be56ca494a5ff59d98828cf385484
jjb4547 <jesus.banuelos.951@my.csun.edu>
2022-04-01 18:11:51 -0700

    Fixed some bugs in Parser.java and added test to ParserTest

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -65,4 +65,58 @@ public class ParserTest {
         assertParses(Arrays.asList(new NumbersToken(6), new MinusToken(), new NumbersToken(2), new MinusToken(), new NumbersToken(1)),
          new ParseResult<Exp>(new OpExp(new OpExp(new IntegerExp(6), new MinusOp(), new IntegerExp(2)), new MinusOp(), new IntegerExp(1)), 0));
     }
+    @Test
+    public void testThreeMultiplications() throws ParserException{
+        assertParses(Arrays.asList(new NumbersToken(6), new TimesToken(), new NumbersToken(2), new DivisionToken(), new NumbersToken(1)),
+         new ParseResult<Exp>(new OpExp(new OpExp(new IntegerExp(6), new MultiplicationOp(), new IntegerExp(2)), new DivisionOp(), new IntegerExp(1)), 0));
+    }
+    @Test
+    public void testLessThan() throws ParserException{
+        assertParses(Arrays.asList(new NumbersToken(6), new LessThanToken(), new NumbersToken(2)),
+         new ParseResult<Exp>(new OpExp(new IntegerExp(6), new LessThanOp(), new IntegerExp(2)), 0));
+    }
+    @Test
+    public void testGreaterThan() throws ParserException{
+        assertParses(Arrays.asList(new NumbersToken(6), new GreaterThanToken(), new NumbersToken(2)),
+         new ParseResult<Exp>(new OpExp(new IntegerExp(6), new GreaterThanOp(), new IntegerExp(2)), 0));
+    }
+    @Test
+    public void testDoubleEquals() throws ParserException{
+        assertParses(Arrays.asList(new NumbersToken(6), new DoubleEqualsToken(), new NumbersToken(2)),
+         new ParseResult<Exp>(new OpExp(new IntegerExp(6), new DoubleEqualsOp(), new IntegerExp(2)), 0));
+    }
+    @Test
+    public void testNotEquals() throws ParserException{
+        assertParses(Arrays.asList(new NumbersToken(6), new NotEqualsToken(), new NumbersToken(2)),
+         new ParseResult<Exp>(new OpExp(new IntegerExp(6), new NotEqualsOp(), new IntegerExp(2)), 0));
+    }
+    @Test 
+    public void checkTrueBool() throws ParserException{
+        assertParses(Arrays.asList(new TrueToken()), new ParseResult<Exp>(new BooleanLiteralExp(true),0));
+    }
+    @Test 
+    public void checkFalseBool() throws ParserException{
+        assertParses(Arrays.asList(new FalseToken()), new ParseResult<Exp>(new BooleanLiteralExp(false),0));
+    }
+    @Test 
+    public void checkParenExp() throws ParserException{
+        assertParses(Arrays.asList(new LeftParenToken(), new NumbersToken(10), new RightParenToken()), new ParseResult<Exp>(new IntegerExp(10),0));
+    }
+    @Test 
+    public void checkStringExp() throws ParserException{
+        assertParses(Arrays.asList(new LeftParenToken(), new StringValueToken("\"hello\""), new RightParenToken()), new ParseResult<Exp>(new StringExp("\"hello\""),0));
+    }
+    @Test 
+    public void checkVariableExp() throws ParserException{
+        assertParses(Arrays.asList(new LeftParenToken(), new VariableToken("i"), new RightParenToken()), new ParseResult<Exp>(new VariableExp("i"),0));
+    }
+    @Test (expected= ParserException.class)
+    public void checkErrorsExp() throws ParserException{
+        assertParses(Arrays.asList(new SemiColonToken()), new ParseResult<Exp>(new VariableExp("i"),0));
+    }
+    @Test (expected= ParserException.class)
+    public void checkErrorExp() throws ParserException{
+        assertParses(Arrays.asList(new LeftParenToken(), new VariableToken("i"), new PeriodToken()), new ParseResult<Exp>(new VariableExp("i"),0));
+    }
+    
 }
